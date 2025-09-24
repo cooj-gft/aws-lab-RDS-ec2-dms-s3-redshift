bea2b1b226328561bca312520c1a490dce3d5cd7
cooj-gft <cooj@gft.com>
2025-09-24 14:44:05 -0500

    Update outputs.tf

--- modules/ec2/outputs.tf
+++ modules/ec2/outputs.tf
@@ -7,8 +7,3 @@ output "private_ip" {
   value       = aws_instance.this.private_ip
   description = "IP privada de la EC2."
 }
-
-output "ec2_sg_id" {
-  value       = aws_security_group.ec2_sg.id
-  description = "ID del security group usado por las instancias EC2 (bastion/ec2)."
-}
